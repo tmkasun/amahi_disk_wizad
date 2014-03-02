b684c18b1a54febf19cd8cf10f6569896aa2eb2f
Kasun Thennakoon <tmkasun@gmail.com>
2014-03-03 00:37:23 +0530

    init commit

---
+++ config/initializers/secret_token.rb
@@ -0,0 +1,12 @@
+# Be sure to restart your server when you modify this file.
+
+# Your secret key is used for verifying the integrity of signed cookies.
+# If you change this key, all old signed cookies will become invalid!
+
+# Make sure the secret is at least 30 characters and all random,
+# no regular words or you'll be exposed to dictionary attacks.
+# You can use `rake secret` to generate a secure secret key.
+
+# Make sure your secret_key_base is kept private
+# if you're sharing your code publicly.
+AmahiDiskManager::Application.config.secret_key_base = '1df06cee3e7d4cd14aaa7c254f29642095689f1183a338b622db0436feccdf6d2cf56b23c616a4aae3e417684490b36f879c8f9222e2d77a731513731b84264a'
---
+++ config/routes.rb
@@ -0,0 +1,8 @@
+AmahiDiskManager::Application.routes.draw do
+  # The priority is based upon order of creation: first created -> highest priority.
+  # See how all your routes lay out with "rake routes".
+
+  # You can have the root of your site routed with "root"
+  # root 'welcome#index'
+  root :to =>  "welcome#index"
+end
