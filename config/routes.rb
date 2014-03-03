3c2d0db96f6f8214c6d04bc9f3fc95a1c2fbdaaf
Kasun Thennakoon <tmkasun@gmail.com>
2014-03-03 16:38:54 +0530

    Add tabs and disk routes

--- config/routes.rb
+++ config/routes.rb
@@ -1,8 +1,15 @@
 AmahiDiskManager::Application.routes.draw do
+  
   # The priority is based upon order of creation: first created -> highest priority.
   # See how all your routes lay out with "rake routes".
 
   # You can have the root of your site routed with "root"
   # root 'welcome#index'
   root :to =>  "welcome#index"
+  scope 'tab/' do
+    
+    resources :disks
+        
+  end
+
 end
