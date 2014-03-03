0533aac7d06817c4767a1006fd4eccbe2cfb39d0
Kasun Thennakoon <tmkasun@gmail.com>
2014-03-03 17:47:13 +0530

    add alert user about data lost

--- app/assets/javascripts/disks.js
+++ app/assets/javascripts/disks.js
@@ -1,2 +1,11 @@
 // Place all the behaviors and hooks related to the matching controller here.
 // All this logic will automatically be available in application.js.
+$(document).ready(function() {
+
+	$('.alert').hide();
+	$('.alert').fadeIn(1500, function() {
+		$(this).delay(6000).fadeOut(1000, function() {
+		});
+	});
+
+});
