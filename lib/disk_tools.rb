8c67a7ba91245635c538f55a0bb824cdf9f48964
Kasun Thennakoon <tmkasun@gmail.com>
2014-03-07 08:55:46 +0530

    Add disk_tools lib to execute system commands and get disks information

---
+++ lib/disk_tools.rb
@@ -0,0 +1,10 @@
+require "disk_tools/version"
+require "disk_tools/engine"
+
+module Wizard
+	class Lib
+		require "disk_tools/disk_utils"
+		# or inside lib/disks/whatever.rb and required here
+	end
+end
+
