e79504edd91d1e5c753aeee8ca788e0272975715
Kasun Thennakoon <tmkasun@gmail.com>
2014-03-24 16:14:47 +0530

    update disk_tools library(previously engine) accordingly to workwith prototype

--- lib/disk_tools.rb
+++ lib/disk_tools.rb
@@ -1,10 +1,8 @@
-require "disk_tools/version"
-require "disk_tools/engine"
-
 module Wizard
 	class Lib
 		require "disk_tools/disk_utils"
-		# or inside lib/disks/whatever.rb and required here
+		require "disk_tools/fstab"
+                # or inside lib/disks/whatever.rb and required here
 	end
 end
 
