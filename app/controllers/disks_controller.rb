4b8ee772d4bde2712e2da55a3457b8592234b740
Kasun Thennakoon <tmkasun@gmail.com>
2014-04-27 01:07:12 +0530

    add validations

--- app/controllers/disks_controller.rb
+++ app/controllers/disks_controller.rb
@@ -8,7 +8,15 @@ class DisksController < ApplicationController
   end
 
   def select_fs
-
+    flash[:error] = "This will completely erase this new drive! Make sure the selected hard drive is the drive you'd like to erase."
+    device = params[:device]
+    partition = params[:partition]
+    unless device or partition
+      flash[:error] = "You should select a Device or a Partition to continue with the Disk-Wizard"
+      redirect_to select_path
+      return false
+    end
+    
   end
 
   def manage_disk
