1b258211f0313b0d81a8dbc31a9d90046510de6b
Kasun Thennakoon <tmkasun@gmail.com>
2014-04-29 02:03:02 +0530

    keep user selections via session variable

--- app/controllers/disks_controller.rb
+++ app/controllers/disks_controller.rb
@@ -1,23 +1,24 @@
 class DisksController < ApplicationController
   layout 'disk_wizard'
-  
   def select_device
     @mounted_disks = Disk.mounts
     @new_disks = Disk.new_disks
-    # render text: "#{@new_disks} </br></br></br></br></br> #{@mounted_disks}"
+  # render text: "#{@new_disks} </br></br></br></br></br> #{@mounted_disks}"
   end
 
   def select_fs
-    flash[:error] = "This will completely erase this new drive! Make sure the selected hard drive is the drive you'd like to erase."
     device = params[:device]
-    partition = params[:partition]
-    unless device or partition
+    format = params[:format]
+    if not device
       redirect_to select_path, :flash => { :error => "You should select a Device or a Partition to continue with the Disk-Wizard" }
-      return false
+    return false
+    elsif not format
+      redirect_to manage_path and return
     end
-    selection = partition || device
-    @selected_disk = Disk.find selection
-    # render text: selected_disk
+    flash[:error] = "This will completely erase this new drive! Make sure the selected hard drive is the drive you'd like to erase."
+    @selected_disk = Disk.find device
+    # puts "DEBUG *************************************#{device}"
+    self.user_selections = {kname: device,format: format}
   end
 
   def manage_disk
@@ -26,6 +27,17 @@ class DisksController < ApplicationController
 
   def done
 
+  end
+
+  def user_selections
+    return JSON.parse session[:user_selections] rescue nil
+  end
+  helper_method :user_selections
+
+  def user_selections=(hash)
+    puts "DEBUG *********************** hash{hash}"
+    session[:user_selections] = hash.to_json
+    puts "DEBUG ************************** session[:user_selections] #{session[:user_selections]}"
   end
 
 end
