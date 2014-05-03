3a86a5f830aa393c33807701938d2eb296c17602
Kasun Thennakoon <tmkasun@gmail.com>
2014-05-03 15:05:58 +0530

    update confirmation page, add progress AJAX

---
+++ app/assets/javascripts/progress.js
@@ -0,0 +1,19 @@
+$(document).ready(function() {
+	setInterval(ajaxd, 2000);
+});
+
+function ajaxd() {
+	$.ajax({
+		type : "GET",
+		dataType : "json",
+		url : "get_progress",
+		// data : "user=success",
+		success : function(progress) {
+			// alert(progress.message + progress.percentage);
+			$("#progress_message").html(progress.message);
+			if (progress.percentage < 0) {
+				window.location.href = "error";
+			};
+		}
+	});
+}
