dcfa765540e96d23a4c34a9f575e4237246adb6c
Kasun Thennakoon <tmkasun@gmail.com>
2014-05-07 23:49:47 +0530

    impliment common disk command executing method

--- lib/parted.rb
+++ lib/parted.rb
@@ -17,24 +17,52 @@
 
 class Parted
   
-  def initialize
-    
-  end
-  
-  def partition_table disk
-    
+  def self.partition_table disk
+    command = "parted #{disk} print"
+    result = disk_command command
+    result.each_line do |line|
+      if line.strip =~ /^Error:/
+        puts "DEBUG:************no disk line#{line}"
+        return false
+      elsif line.strip =~ /^Partition Table:/
+        #TODO: Need to test for all the types of partition tables
+        table_type = line.match(/^Partition Table:(.*)/i).captures[0].strip
+        puts "DEBUG:************line#{table_type}"
+        return table_type
+      end
+    end
   end
   
   def format device, fs_type
     
   end
   
-  def create_table type
+  def create_partition_table device , type = 'msdos'
+    command = 'parted -script #{device} #{type}'
+    result = disk_command command
+    result.each_line do |line|
+      if line.strip =~ /^Error:/
+        puts "DEBUG:************no disk line#{line}"
+        return false
+      end
+    end
+  end
+  
+  def create_fs device, fs_type
     
   end
   
-  def create_fs fs_type
-    
+  private
+
+  def disk_command command, blocking = true
+    #forward command result(stdio and stderror) to temp file default location for temp file is /var/hda/tmp
+    if blocking # default mode is blocking call, because other commands down the line depend on the result of the previous command(i.e. format after partitioning)
+      Command.new("#{command} > /tmp/disk_wizard.tmp 2>&1").run_now #.execute is kind of none-blocking call and  run_now is a blocking call
+    else
+      Command.new("#{command} > /tmp/disk_wizard.tmp 2>&1").execute
+    end
+    result = File.open("/tmp/disk_wizard.tmp", "r").read
+    return result
   end
       
 end
